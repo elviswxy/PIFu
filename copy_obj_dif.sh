600b96f09623936e50b95128fa948e36a07f33a5
elviswxy <elviswu0306@gmail.com>
2020-11-12 17:44:43 -0800

    update

--- copy_obj_dif.sh
+++ copy_obj_dif.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 file_list=$(ls /hd4/renderpeople/models/prt_data)
-for file in $file_list
+for file in ${file}_list
 do
-		if [ ! -f "/hd4/renderpeople/models/unzip_models/$file_MAYA/$file_MAYA/scenes/$file_100k.obj" ]
+		if [ ! -f "/hd4/renderpeople/models/unzip_models/${file}_MAYA/${file}_MAYA/scenes/${file}_100k.obj" ]
 		then
-			cp /hd4/renderpeople/models/unzip_models/$file_MAYA/$file_MAYA/scenes/$file_100k.obj /hd4/renderpeople/models/prt_data/$file/
-			cp /hd4/renderpeople/models/unzip_models/$file_MAYA/$file_MAYA/sourceimages/$file/$file_dif_8k.jpg /hd4/renderpeople/models/prt_data/$file/
-			cp /hd4/renderpeople/models/unzip_models/$file_MAYA/$file_MAYA/sourceimages/$file/$file_dif_2k.jpg /hd4/renderpeople/models/prt_data/$file/
+			cp /hd4/renderpeople/models/unzip_models/${file}_MAYA/${file}_MAYA/scenes/${file}_100k.obj /hd4/renderpeople/models/prt_data/${file}/
+			cp /hd4/renderpeople/models/unzip_models/${file}_MAYA/${file}_MAYA/sourceimages/${file}/${file}_dif_8k.jpg /hd4/renderpeople/models/prt_data/${file}/
+			cp /hd4/renderpeople/models/unzip_models/${file}_MAYA/${file}_MAYA/sourceimages/${file}/${file}_dif_2k.jpg /hd4/renderpeople/models/prt_data/${file}/
 			else
 			  echo $file
 		fi
