16189b23d0e51564b172620881b514cd1760d6c3
elviswxy <elviswu0306@gmail.com>
2020-11-12 17:57:34 -0800

    update

--- copy_obj_dif.sh
+++ copy_obj_dif.sh
@@ -6,8 +6,8 @@ do
 		if [ -f "/hd4/renderpeople/models/unzip_models/${file}_MAYA/${file}_MAYA/${file}_MAYA/scenes/${file}_100k.obj" ]
 		then
 			cp "/hd4/renderpeople/models/unzip_models/"$file"_MAYA/"$file"_MAYA/"$file"_MAYA/scenes/"$file"_100k.obj" /hd4/renderpeople/models/prt_data/$file/
-			cp "/hd4/renderpeople/models/unzip_models/"$file"_MAYA/"$file"_MAYA/"$file"_MAYA/sourceimages/"$file"/"$file"_dif_8k.jpg" /hd4/renderpeople/models/prt_data/$file/
-			cp "/hd4/renderpeople/models/unzip_models/"$file"_MAYA/"$file"_MAYA/"$file"_MAYA/sourceimages/"$file"/"$file"_dif_2k.jpg" /hd4/renderpeople/models/prt_data/$file/
+			cp "/hd4/renderpeople/models/unzip_models/"$file"_MAYA/"$file"_MAYA/"$file"_MAYA/sourceimages/"$file"_dif_8k.jpg" /hd4/renderpeople/models/prt_data/$file/
+			cp "/hd4/renderpeople/models/unzip_models/"$file"_MAYA/"$file"_MAYA/"$file"_MAYA/sourceimages/"$file"_dif_2k.jpg" /hd4/renderpeople/models/prt_data/$file/
 			else
 			  echo $file
 		fi
