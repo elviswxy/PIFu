28e4365eb1b81771f7cc12651292f46229855c10
elviswxy <elviswu0306@gmail.com>
2020-11-13 00:20:20 -0800

    image

--- imgs_script.sh
+++ imgs_script.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-file_list=$(ls /hd4/renderpeople/models/unzip_models)
+file_list=$(ls /hd4/renderpeople/models/prt_data)
 for file in $file_list
 do
-	for letter in {g..i}
-	do
-		if [[ $file =~ rp_$$letter ]]
-		then
-			echo $$file
-			python -m apps.prt_util -i /hd4/renderpeople/models/unzip_models/$$file/$$file/scenes
-		fi
-	done
+  python -m apps.render_data -i /hd4/renderpeople/models/prt_data/$file -o /hd4/renderpeople/models/imgs_data -e
 done
