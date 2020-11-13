579bb28751f12f12c2b7ed055946c3f7be2630ca
elviswxy <elviswu0306@gmail.com>
2020-11-12 17:58:38 -0800

    update

--- copy_obj_dif.sh
+++ copy_obj_dif.sh
@@ -3,11 +3,11 @@
 file_list=$(ls /hd4/renderpeople/models/prt_data)
 for file in $file_list
 do
-		if [ -f "/hd4/renderpeople/models/unzip_models/${file}_MAYA/${file}_MAYA/${file}_MAYA/scenes/${file}_100k.obj" ]
+		if [ -f "/hd4/renderpeople/models/unzip_models/${file}_MAYA/${file}_MAYA/scenes/${file}_100k.obj" ]
 		then
-			cp "/hd4/renderpeople/models/unzip_models/"$file"_MAYA/"$file"_MAYA/"$file"_MAYA/scenes/"$file"_100k.obj" /hd4/renderpeople/models/prt_data/$file/
-			cp "/hd4/renderpeople/models/unzip_models/"$file"_MAYA/"$file"_MAYA/"$file"_MAYA/sourceimages/"$file"_dif_8k.jpg" /hd4/renderpeople/models/prt_data/$file/
-			cp "/hd4/renderpeople/models/unzip_models/"$file"_MAYA/"$file"_MAYA/"$file"_MAYA/sourceimages/"$file"_dif_2k.jpg" /hd4/renderpeople/models/prt_data/$file/
+			cp "/hd4/renderpeople/models/unzip_models/"$file"_MAYA/"$file"_MAYA/scenes/"$file"_100k.obj" /hd4/renderpeople/models/prt_data/$file/
+			cp "/hd4/renderpeople/models/unzip_models/"$file"_MAYA/"$file"_MAYA/sourceimages/"$file"_dif_8k.jpg" /hd4/renderpeople/models/prt_data/$file/
+			cp "/hd4/renderpeople/models/unzip_models/"$file"_MAYA/"$file"_MAYA/sourceimages/"$file"_dif_2k.jpg" /hd4/renderpeople/models/prt_data/$file/
 			else
 			  echo $file
 		fi
