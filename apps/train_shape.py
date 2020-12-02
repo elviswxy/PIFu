fa93cfbaa03555bce2ed2cae55816c772a7fdf77
elviswxy <elviswu0306@gmail.com>
2020-12-01 19:20:39 -0800

    update model

--- apps/train_shape.py
+++ apps/train_shape.py
@@ -1,6 +1,5 @@
 import sys
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '5,6'
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
